ddd648f2a1b214c6eb1ed7bc6ffd04bbb395db36
marynmcnamara <119798224+marynmcnamara@users.noreply.github.com>
2023-02-27 10:49:18 -0500

    created react app

---
+++ src/WeatherSearch.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchCity(city);
+  }
+
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+
+  function searchCity(city) {
+    const apiKey = "5283f64953895afdf452cd2ee9d7be1e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  let form = (
+    <div className="Search-form">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city"
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul class="items">
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
