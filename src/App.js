35e21a25dab6add2b595bc71f511bd25f6ad9fe0
marynmcnamara <119798224+marynmcnamara@users.noreply.github.com>
2023-02-27 10:55:00 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search</h1>
-      <WeatherSearch />
+      <div>
+        <h1>Weather Search</h1>
+        <WeatherSearch />
+      </div>
+      <footer>
+        <a
+          href="https://github.com/marynmcnamara/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Maryn McNamara
+      </footer>
     </div>
   );
 }
